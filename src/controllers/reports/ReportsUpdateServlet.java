fcc8173a20480da5aa1c1961995f2c831f33f18e
keisuke.suzuki <mokyuzero.spirituals@icloud.com>
2020-02-22 01:03:58 +0900

    Modify Report update

--- src/controllers/reports/ReportsUpdateServlet.java
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -45,6 +45,9 @@ public class ReportsUpdateServlet extends HttpServlet {
             r.setReport_date(Date.valueOf(request.getParameter("report_date")));
             r.setTitle(request.getParameter("title"));
             r.setContent(request.getParameter("content"));
+
+            r.setBusiness_situation(request.getParameter("business_situation"));
+
             r.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 
             List<String> errors = ReportValidator.validate(r);
