f8622e557f08946e4e59a2bfa438c897335eb8a9
keisuke.suzuki <mokyuzero.spirituals@icloud.com>
2020-02-21 14:42:41 +0900

    Add Customer class

---
+++ src/models/Customer.java
@@ -0,0 +1,99 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "customers")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllCustomers",
+            query = "SELECT r FROM Customer AS r ORDER BY r.id DESC"
+            ),
+    @NamedQuery(
+            name = "getCustomersCount",
+            query = "SELECT COUNT(r) FROM Customer AS r"
+            ),
+})
+@Entity
+public class Customer {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "customer_name", length = 255, nullable = false)
+    private String customer_name;
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_naem(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
---
+++ src/models/validators/CustomerValidator.java
@@ -0,0 +1,41 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Customer;
+
+public class CustomerValidator {
+    public static List<String> validate(Customer r) {
+        List<String>errors = new ArrayList<String>();
+
+        String customer_name_error = _validateCustomer_name(r.getCustomer_name());
+        if(!customer_name_error.equals("")) {
+            errors.add(customer_name_error);
+        }
+
+        String content_error = _validateContent(r.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+
+    private static String _validateCustomer_name(String customer_name) {
+        if(customer_name == null || customer_name.equals("")) {
+            return "顧客名を入力してください。";
+        }
+        return "";
+    }
+
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "顧客情報を入力してください。";
+        }
+        return "";
+    }
+
+
+}
